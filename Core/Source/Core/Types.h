a10bb339e9bbcc355879917f211709312bdf5a15
Icd-Kohi <luambo13@gmail.com>
2026-06-13 18:28:57 -0300

    ADD: Init Types

---
+++ Core/Source/Core/Types.h
@@ -0,0 +1,19 @@
+#ifndef TYPES
+#define TYPES
+
+#include <iostream>
+#include <cctype>
+#include <cstdint>
+#include <cstring>
+#include <sys/types.h>
+#include <fstream>
+
+#define SIZE(obj) sizeof(obj)/sizeof(obj[0])
+typedef u_int8_t u8;
+typedef u_int16_t u16;
+typedef u_int32_t u32;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+
+#endif

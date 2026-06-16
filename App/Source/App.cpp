5ed5dbcd2874c22a7616d8dbf45542b14abf46b1
Icd-Kohi <luambo13@gmail.com>
2026-06-16 19:54:30 -0300

    ...

--- App/Source/App.cpp
+++ App/Source/App.cpp
@@ -1,24 +1,17 @@
-#include "../../Core/Source/Core/Core.h"
 #include "../../Core/Source/Core/Chip8.h"
 #include "../../Core/Source/Core/Types.h"
-
-int Core::main(){
-    int thing = 0;
-    return thing;
-};
 
 u8 hey();
 
 int main()
 {
-	//Core::Printing();
     
     Chip8::Chip8* thing;
     Chip8::Chip8 thing2;
 
     std::cout << "Checking...\n";
 
-    return Core::main();
+    return 0;
 }
 
 void process(char *s){
